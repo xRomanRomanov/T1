8e9fed896ec1a50f5229b2c0c6144cb9c8d3cff0
Роман <@DESKTOP-78FMSI2>
2022-08-31 15:01:39 +0300

    Добавьте файлы проекта.

---
+++ T1/T1.cpp
@@ -0,0 +1,27 @@
+﻿// T1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+using std::cout;
+using std::cin;
+
+
+int main()
+{
+    std::cout << "Hello World!\n";
+
+    std::cout << "test GitHub\n";
+
+    std::cout << "This version is main" << std::endl;
+
+
+    //задержка выключения
+    cin.clear();
+    cin.ignore(32000, '\n');
+    cin.get();
+    return 0;
+
+}
+
+
